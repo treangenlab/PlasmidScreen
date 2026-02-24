a7238b698f9ead7d4eb4f793eca38a4630a9554d
FelixMQuintana <fmq1@rice.edu>
2026-02-24 07:43:57 -0600

    Removed more code not needed and reformatted CLI

--- plasmidScreen.py
+++ plasmidScreen.py
@@ -1,14 +1,11 @@
 import typer
 import logging
 
-from pathlib import Path
 from rich.logging import RichHandler
 from typing import Annotated
 
-from plasmidScreen.lib.const import KrakenConfig, MetaMDBGConfig, MegahitConfig
 from plasmidScreen.lib.funcs import get_default_db_path
-from plasmidScreen.src.kraken.build_db import BuildDB
-from plasmidScreen.src.workflow.workflow import LRWorkflow, SRWorkflow
+from plasmidScreen.src.plasmidScreen import Workflow
 
 FORMAT = "%(message)s"
 logging.basicConfig(level=logging.INFO, format=FORMAT, datefmt="[%X]", handlers=[RichHandler()])
@@ -16,59 +13,28 @@ app = typer.Typer(pretty_exceptions_show_locals=False,
                   help="Welcome to PlasmidScreen. A k-mer based tool to detect"
                        " engineered DNA.")
 
-DEFAULT_DB_PATH = get_default_db_path("KrakenEng")
+DEFAULT_DB_PATH = get_default_db_path("PlasmidScreen")
+
+
+@app.command()
+def screen(ctx: typer.Context, fasta_file: Annotated[str, typer.Argument(help="Fasta file ")],
+           output_report_path: Annotated[str, typer.Argument(help="Engineered KrakenDB")],
+           kraken_raw_output: Annotated[str, typer.Argument(help="Raw kraken output path")],
+           window_size: Annotated[int, typer.Option("--window_size",
+                                                    help="Window size to scan for engineered k-mers")] = 200,
+           engineered_k_mer_threshold: Annotated[int, typer.Option("--threshold", help="Threshold of required "
+                                                                                       "identified engineered DNA.")] = 25,
+           kraken_db_path: Annotated[str, typer.Argument(help="Engineered KrakenDB")] = DEFAULT_DB_PATH,
+           threads: Annotated[int, typer.Option("--threads", help="Available threads to use.")] = 4,
+           verbose: Annotated[bool, typer.Option("--verbose", "-v", help="Enable verbose logging.")] = False
+           ):
+    Workflow(fasta_file, output_report_path, kraken_db_path, threads, kraken_raw_output, window_size,
+             engineered_k_mer_threshold).run()
 
 
 @app.callback()
-def main(ctx: typer.Context,
-         verbose: Annotated[bool, typer.Option("--verbose", "-v",
-                                               help="Enable verbose logging.")] = False):
-    config = KrakenConfig()
-    ctx.obj = config
-
-
-@app.command()
-def short_read(ctx: typer.Context, fasta_file: Annotated[str, typer.Argument(help="Fasta file ")],
-               paired_end: Annotated[str, typer.Option(default="", help="Fasta file of paired end")],
-               assembly: Annotated[bool, typer.Argument(help="Run on raw reads or assemble contigs")],
-               output_report_path: Annotated[str, typer.Argument(help="Engineered KrakenDB")],
-               kraken_db_path: Annotated[str, typer.Argument(help="Engineered KrakenDB")] = DEFAULT_DB_PATH):
-    config: KrakenConfig = ctx.obj
-    meta_config = MegahitConfig()
-    SRWorkflow(fasta_file=fasta_file, paired_end=paired_end, output_report_path=output_report_path,
-               kraken_db=kraken_db_path, kraken_config=config, assembly=assembly,
-               assembly_config=meta_config).run_kraken()
-
-
-@app.command()
-def long_read(ctx: typer.Context, fasta_file: Annotated[str, typer.Argument(help="Fasta file ")],
-              assembly: Annotated[bool, typer.Argument(help="Run on raw reads or assemble contigs")],
-              output_report_path: Annotated[str, typer.Argument(help="Engineered KrakenDB")],
-              kraken_raw_output: Annotated[str, typer.Argument(help="Raw kraken output path")] ,
-              kraken_db_path: Annotated[str, typer.Argument(help="Engineered KrakenDB")] = DEFAULT_DB_PATH,):
-    kraken_config: KrakenConfig = ctx.obj
-    meta_config = MetaMDBGConfig()
-    LRWorkflow(fasta_file=fasta_file, output_report_path=output_report_path, kraken_db=kraken_db_path,
-               kraken_raw_output=kraken_raw_output,
-               kraken_config=kraken_config, assembly=assembly,
-               assembly_config=meta_config).run_kraken()
-
-
-@app.command(help="Build custom kraken database to build k-mer database. Recommend to add E. Coli")
-def build_kraken_db(ctx: typer.Context,
-                    natural_fastas: Annotated[str, typer.Argument(help="Fasta file for natural sequences")],
-                    engineered_fastas: Annotated[str, typer.Argument(help="Fasta file for natural sequences")],
-                    additional_fastas: Annotated[str, typer.Option(help="Fasta file of additional sequences for "
-                                                                        "higher engineered sequence sensitivity. "
-                                                                        "Recommended adding E. Coli genomes "
-                                                                        "but will add runtime.")] = None,
-                    db_name: Annotated[str, typer.Argument(help="Name of Custom kraken DB")] = "engineered_kraken_db"
-                    ):
-    config: KrakenConfig = ctx.obj
-    build_db_obj = BuildDB(Path(DEFAULT_DB_PATH), db_name, natural_fastas, engineered_fastas,
-                           additional_fastas,
-                           config)
-    build_db_obj.build_kraken_db()
+def main():
+    pass
 
 
 if __name__ == "__main__":
--- plasmidScreen/src/plasmidScreen.py
+++ plasmidScreen/src/plasmidScreen.py
@@ -1,25 +1,23 @@
 """
 
 """
-import abc
 import logging
 import subprocess
 import sys
 from pathlib import Path
 from tqdm import tqdm
-from plasmidScreen.lib.const import KrakenConfig, MetaMDBGConfig, MegahitConfig
 import numpy as np
 from numba import jit
 
 
 @jit(nopython=True)
-def fast_window_logic(tids, counts):
-    window_size = 200
-    overlap_max = 20
+def fast_window_logic(tids, counts, window_size: int, threshold: int):
+    #window_size = 200
+    #threshold = 20
 
-   # window_size = 500
+    # window_size = 500
     target_tid = 32630  # Use int for speed comparisons
-   # overlap_max = 15
+    # overlap_max = 15
     max_kmers = window_size - 21 + 1
 
     # Local variables (Faster than dicts)
@@ -63,44 +61,43 @@ def fast_window_logic(tids, counts):
                         eng_count = 0
 
             # Check threshold
-            if eng_count >= overlap_max:
+            if eng_count >= threshold:
                 return True
 
     return False
 
 
-class Workflow(abc.ABC):
+class Workflow:
 
-    def __init__(self, fasta_file: str, output_report_path: str, kraken_db: str, kraken_raw_output: str,
-                 kraken_config: KrakenConfig, window_size:int = 200, engineered_kmer_threshold: int = 15 ):
+    def __init__(self, fasta_file: str, output_report_path: str, kraken_db: str, threads: int, kraken_raw_output: str,
+                 window_size: int = 200, engineered_kmer_threshold: int = 15):
         self.fasta_file: Path = Path(fasta_file)
         self.report_output_path = Path(output_report_path)
         self.kraken_db = Path(kraken_db)
-        self.kraken_config = kraken_config
         self.kraken_output_path = kraken_raw_output
         self.threshold = engineered_kmer_threshold
         self.window_size = window_size
+        self.max_threads = threads
 
     def run_kraken(self, ):
         try:
             logging.info(f"Running Kraken on {self.fasta_file}")
-                            #output_file_path: Path = self.kraken_db.parent.joinpath("output.txt")
             report_file_path: Path = self.kraken_db.parent.joinpath("report.txt")
-        #    logging.info(f"Running the command: kraken2 --db {self.kraken_db} --report-minimizer-data "
-        #                 f"--report {report_file_path} --output {self.kraken_output_path} --use-names {self.fasta_file} "
-        #                 f"--threads {self.kraken_config.max_threads}")
-        #    subprocess.check_call(
-        #        ["kraken2", "--db", self.kraken_db, "--report-minimizer-data", "--report", report_file_path,
-        #         "--output", self.kraken_output_path, "--use-names", self.fasta_file,
-        #         "--threads", str(self.kraken_config.max_threads)])
-        #except subprocess.CalledProcessError:
-        #    logging.info(f"CRITICAL ERROR: Failed to run kraken2 {self.fasta_file}.")
-        #    sys.exit(1)
+            logging.info(f"Running the command: kraken2 --db {self.kraken_db} --report-minimizer-data "
+                         f"--report {report_file_path} --output {self.kraken_output_path} --use-names {self.fasta_file} "
+                         f"--threads {self.max_threads}")
+            subprocess.run(
+                ["kraken2", "--db", self.kraken_db, "--report-minimizer-data", "--report", report_file_path,
+                 "--output", self.kraken_output_path, "--use-names", self.fasta_file,
+                 "--threads", str(self.max_threads)], check=True, capture_output=True, text=True)
+        except subprocess.CalledProcessError as e:
+            # Now you can log the exact error message Kraken2 spit out
+            logging.error(f"Kraken2 failed (Code {e.returncode}). Error details: {e.stderr}")
+            sys.exit(1)
         logging.info("All files processed.")
-        self._post_process_kraken()
 
     @staticmethod
-    def parse_and_run(kmer_pos_info: str):
+    def parse_and_run(kmer_pos_info: str, window_size: int, threshold: int):
         # Quick string parsing to get integer arrays
         # This replaces the slow item.split(':') inside the loop
         raw_data = kmer_pos_info.replace('|:|', '').split()
@@ -112,37 +109,27 @@ class Workflow(abc.ABC):
 
         for i, item in enumerate(raw_data):
             t, c = item.split(':')
-            if t=='A':
+            if t == 'A':
                 tids[i] = -1
             else:
                 tids[i] = int(t)
             counts[i] = int(c)
 
-        return fast_window_logic(tids, counts)
+        return fast_window_logic(tids, counts, window_size, threshold)
 
-    @abc.abstractmethod
-    def _rescue_logic(self) -> bool:
-        raise NotImplementedError
-
-    def _post_process_kraken(self):
+    def scan_engineered_blocks_kraken(self):
         report = open(self.report_output_path, "w")
         with open(self.kraken_output_path, 'r') as kraken_file:
             entries = kraken_file.readlines()
             for entry in tqdm(entries, total=len(entries)):
                 categories = entry.split("\t")
-                synthetic_boolean: bool = self.parse_and_run(categories[-1])
+                synthetic_boolean: bool = self.parse_and_run(categories[-1], self.window_size, self.threshold)
                 logging.debug(f"Entry: {categories[1]} found to be synthetic from windowing logic: {synthetic_boolean}")
                 if synthetic_boolean:
                     report.write(f"Synthetic\t{categories[1]}\n")
                 else:
-                    synthetic_boolean = self._rescue_logic()
-                    logging.debug(
-                        f"Entry: {categories[1]} found to be synthetic from rescue logic: {synthetic_boolean}")
-                    if synthetic_boolean:
-                        report.write(f"Synthetic\t{categories[1]}\n")
-                    else:
-                        report.write(f"Natural\t{categories[1]}\n")
+                    report.write(f"Natural\t{categories[1]}\n")
 
     def run(self):
         self.run_kraken()
-        self._post_process_kraken()
+        self.scan_engineered_blocks_kraken()
